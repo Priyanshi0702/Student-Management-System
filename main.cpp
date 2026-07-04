cc80945e4245b22459f647530d3e5c8785d5695e
priyanshi0702 <priyanshi07022007@gmail.com>
2026-07-04 15:37:15 +0530

    Implement addStudent using file handling

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
 class Student
 {
@@ -9,30 +10,97 @@ class Student
     float cgpa;
     public:
     void getdata()
-    {
+    {   
         cout<<"Enter roll number: ";
         cin>>roll;
-        cout<<"Enter name: ";
-        cin>>name;
+        cin.ignore();
+        cout<<"Enter name:";
+        getline(cin,name);
         cout<<"Enter marks: ";
         cin>>mks;
         cout<<"enter CGPA:";
         cin>>cgpa;
+        
     }
     void showdata()
-    {
-        cout<<roll<<" ";
-        cout<<name<<"  ";
+    {   
+        cout<<roll<<" "; 
+        cout<<name<<" ";
         cout<<mks<<" ";
-        cout<<cgpa<<" ";
+        cout<<cgpa<<" "<<"\n";
+        
     }
-};
-int main()
+    void setroll(int r)
+    {
+        roll = r;
+    }
+    void setname(string n)
+    {
+        name = n;
+    }
+    void setmarks(int m)
+    {
+        mks = m;
+    }
+    void setcgpa(float c)
+    {
+        cgpa = c;
+    }
+    int getroll()
 {
-    
+    return roll;
+}
+
+string getname()
+{
+    return name;
+}
+
+int getmarks()
+{
+    return mks;
+}
+
+float getcgpa()
+{
+    return cgpa;
+}
+};
+class StudentManagement
+{
+    public:
+    void addstudent();
+    void searchstudent();
+    void updatestudent();
+    void deletestudent();
+};
+void StudentManagement::addstudent()
+{
     Student s;
+    cout<<"Enter student details: "<<endl;
     s.getdata();
-    s.showdata();
+    ofstream fout ("student.txt",ios::app);
+    if (!fout)
+{
+    cout << "Error opening file!\n";
+    return;
+}
+    fout<<s.getroll()<<" "<<s.getname()<<" "<<s.getmarks()<<" "<<s.getcgpa()<<" "<<"\n";
+    fout.close();
+    cout<<"New student added successfully, \n";
+    
+}
+
+int main()
+{   
+    cout<<"enter the number of students you want to add: ";
+    int n;
+    cin>>n;
+    StudentManagement obj1;
+    for(int i=1; i<=n; i++)
+    {
+        obj1.addstudent();
+    }
     return 0;
 }
 
